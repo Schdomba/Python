b0d763d48d6b3d7dbfd6948e3a01e5d728ae75e9
unknown <stumppyannik@gmail.com>
2020-04-05 18:08:32 +0200

    add GUI
    	- add GUI of the board
    	- add GUI of possible values

--- sudoku/GUI.py
+++ sudoku/GUI.py
@@ -5,12 +5,16 @@ from sudoku import Sudoku
 
 pygame.init()
  
-display_width = 800
+display_width = 600
 display_height = 600
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Sudoku')
 
-sd = Sudoku(2) #an instance of sudoku, size changes later
+sd = 0 #an instance of sudoku
+subsecSize = 0 #width or height of a subsection (in tiles)
+boardSize = 0 #width or height of the board (in tiles)
+
+tilePos = [] #list of tile positions (top left corner)
 
 largeText = pygame.font.Font('freesansbold.ttf',50)
 mediumText = pygame.font.Font('freesansbold.ttf', 25)
@@ -93,20 +97,75 @@ def game_intro():
         pygame.display.update()
         clock.tick(15)
         
-        
-    
+def drawLines():
+    #position of thick line (between subsections)
+    thickPosx = 0
+    thickPosy = 0
+    #outer loop for thick lines
+    for i in range(1, subsecSize+1):
+        #inner loop for thin lines
+        for j in range (1, subsecSize):
+            #draw thin lines
+            pygame.draw.line(gameDisplay,black,(thickPosx+j*display_width/boardSize,0),(thickPosx+j*display_width/boardSize,display_height))
+            pygame.draw.line(gameDisplay,black,(0,thickPosy+j*display_height/boardSize),(display_width,thickPosy+j*display_height/boardSize))
+        #don't draw thick line in last iteration
+        if(i==subsecSize):
+            break
+        #next position of thick lines
+        thickPosx = i*display_width/subsecSize
+        thickPosy = i*display_height/subsecSize
+        #draw thick lines
+        pygame.draw.line(gameDisplay,black,(thickPosx,0),(thickPosx,display_height),4)
+        pygame.draw.line(gameDisplay,black,(0,thickPosy),(display_width,thickPosy),4)
+
+
+def drawTilePossib(possib, pos):
+    possib_width = display_width/(boardSize*subsecSize)
+    possib_height = display_height/(boardSize*subsecSize)
+    xoffset = pos[0]+possib_width/2
+    yoffset = pos[1]+possib_height/2
+    blitted = []
+
+    for i in range(1, subsecSize+1):
+        for j in range(subsecSize):
+            if (i+subsecSize*j) in possib:
+                temp = list(text_objects(str(i+subsecSize*j), smallText, grey)) + [i+subsecSize*j]
+                temp[1].center = (((i-1)*possib_width+xoffset), (j*possib_height+yoffset))
+                blitted.append(gameDisplay.blit(temp[0], temp[1]))
+    return blitted
+               
+def drawPossib():
+    possib = sd.getPossib()
+    for i in range(len(tilePos)):
+        drawTilePossib(possib[i], tilePos[i])
     
 #TBD!
 def game_loop():
     print("in game_loop")
     print(sd.getBoard())
+    sd.writeTile(0,0,0,0,1)
+    print(sd.getBoard())
+    global subsecSize
+    global boardSize
+    global tilePos
+    subsecSize = sd.size
+    boardSize = subsecSize*subsecSize
+    for i in range(boardSize):
+        for j in range(boardSize):
+            tilePos.append([i*display_width/boardSize, j*display_height/boardSize])
+    print(tilePos)
 
     gameExit = False
     
     while not gameExit:
+        #always refresh the display in the beginning                
+        gameDisplay.fill(white)
+        drawLines()
+        drawPossib()
         #get the length of board, get the square root to get height/width
         #create rectangles in a loop
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -116,7 +175,6 @@ def game_loop():
                 #check which tile has been clicked
                 #mark this tile
                 #let the player enter a number
-        gameDisplay.fill(white)
         pygame.display.update()
         clock.tick(60)
 
